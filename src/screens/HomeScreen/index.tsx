990b7883a70994cd05fd4761ca52ee61b4d4ddf9
Dilawar <dilawarjavaid26@gmail.com>
2023-09-19 19:05:42 +0500

    feat(screens):add Homescreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,13 +1,14 @@
-import Layout from '@/theme/Layout';
+import AppHeader from '@/components/AppHeader';
+import {Layout} from '@/theme';
 import {GenericNavigation} from '@/types/Navigation';
 import * as React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 const HomeScreen = (props: GenericNavigation) => {
   return (
-    <SafeAreaView style={Layout.fill}>
-      <Text>asjdhbaibdaihbsdjahbdjhasb</Text>
-    </SafeAreaView>
+    // <SafeAreaView style={Layout.fill}>
+    <AppHeader title="Pop Movies" onMorePress={() => {}} />
+    // </SafeAreaView>
   );
 };
 
